c6831e8f2929bbbaff68a8b37b9f45346098b62a
fr <fr@MacBook-Air-de-fr.local>
2024-07-16 23:37:40 +0200

    correcttion des petits bugs dans Login

--- front/src/containers/Login.js
+++ front/src/containers/Login.js
@@ -40,10 +40,16 @@ export default class Login {
   handleSubmitAdmin = e => {
     e.preventDefault()
     const user = {
+
+      ///////////////////////
+      
+      //rempacement de employee par Admin
       type: "Admin",
       email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
       password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       
+      //////////////////////
+
       // email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
       // password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
       
